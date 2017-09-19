0e4d5ce04ac8134eff3fcffab8daef8bf538625b
Scott Woodend <woodendscott@gmail.com>
2017-09-18 21:55:51 -0400

    bring in the homepage

--- react-ui/src/index.js
+++ react-ui/src/index.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Home from './Home';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+  ReactDOM.render((
+    <Router>
+      <div>
+        {/*<Navbar/>*/}
+        <div style={{"margin-top":"55px"}}>
+          <Route exact path="/" component={Home}/>
+          {/*
+          <Route path="/signup" component={Signup}/>
+          <Route path="/login" component={Login}/>
+          <Route path="/dashboard" component={Dashboard}/>
+          <Route path="/new" component={NewPoll}/>
+          <Route path="/polls/:pollId" component={ViewPoll}/>
+          */}
+        </div>
+      </div>
+    </Router>),
+    document.getElementById('root')
+  );
 );
