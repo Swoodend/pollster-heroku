69afa9e038eb8a9a6f3ff3df8024e43d40b498c7
Scott Woodend <woodendscott@gmail.com>
2017-09-18 22:29:45 -0400

    fix imports

--- react-ui/src/index.js
+++ react-ui/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import Home from './Home';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './index.css';
 
 
