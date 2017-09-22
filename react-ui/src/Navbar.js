aa8dd757e15492540b125abb8ef4eb1dc824670a
Scott Woodend <woodendscott@gmail.com>
2017-09-22 00:08:54 -0400

    dont make ajax call for navbar

--- react-ui/src/Navbar.js
+++ react-ui/src/Navbar.js
@@ -11,23 +11,28 @@ class Navbar extends Component {
   }
 
   componentWillMount(){
-    let token = localStorage.getItem('jwt');
-    if (token){
-      fetch(`/validate/${token}`)
-        .then((res) => {
-          return res.json();
-        })
-        .then((token) => {
-          if (token && token.user){
-            this.setState({
-              loggedIn : token.user
-            });
-          }
-        })
+    let user = localStorage.getItem("currentUser");
+    if (user){
+
+      this.setState({
+        loggedIn:user
+      })
+      // fetch(`/validate/${token}`)
+      //   .then((res) => {
+      //     return res.json();
+      //   })
+      //   .then((token) => {
+      //     if (token && token.user){
+      //       this.setState({
+      //         loggedIn : token.user
+      //       });
+      //     }
+      //   })
     }
 
   }
   render(){
+    console.log('rendering nav', this.state);
     let nav = this.state.loggedIn ? <NavbarLoggedIn username={this.state.loggedIn}/> : <NavbarNotLoggedIn/>
     return (
       <div>
