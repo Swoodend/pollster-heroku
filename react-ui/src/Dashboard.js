c21ef14e33a64d38fa7748bc4b7a3b10a1200d62
Scott Woodend <woodendscott@gmail.com>
2017-09-21 23:37:51 -0400

    add comma

--- react-ui/src/Dashboard.js
+++ react-ui/src/Dashboard.js
@@ -8,7 +8,7 @@ class Dashboard extends Component {
   constructor(props){
     super(props);
     this.state = {
-      loggedIn: false
+      loggedIn: false,
       currentUserPolls: []
     }
   }
